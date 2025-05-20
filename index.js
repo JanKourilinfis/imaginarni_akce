e259afcfb01b79312d3b8bf621cc12017768beae
unknown <jan.kouril@infis.cz>
2025-05-20 12:24:16 +0200

    AYAYAY

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 function odeslat(){
     const text=document.getElementById("text").value
     alert(text)
+    console.log(text)
+    const email=document.getElementById("email").value
+    alert(email)
+    console.log(email)
+    const telefon=document.getElementById("telefon").value
+    alert(telefon)
+    console.log(telefon)
+    const narozeni=document.getElementById("narozeni").value
+    alert(narozeni)
+    console.log(narozeni)
     const color=document.getElementById("color").value
     alert(color)
+    console.log(color)
+    const triko=document.getElementById("triko").value
+    alert(triko)
+    console.log(triko)
+    const tema=document.getElementById("tema").value
+    alert(tema)
+    console.log(tema)
+    const nastup=document.getElementById("nastup").value
+    alert(nastup)
+    console.log(nastup)
+    const uroven=document.getElementById("uroven").value
+    alert(uroven)
+    console.log(uroven)
+    const web=document.getElementById("web").value
+    alert(web)
+    console.log(web)
 }
